b2e529fa8a97d376e2d15ccd0b680ab725ae714c
brayanx16 <brayanx16@outlook.com>
2023-03-27 01:22:27 -0300

    Handling pagination

--- src/components/ListRestaurants/index.tsx
+++ src/components/ListRestaurants/index.tsx
@@ -3,20 +3,37 @@ import IRestaurant from "../../interfaces/IRestaurant";
 import style from "./ListRestaurant.module.scss";
 import Restaurant from "./Restaurant";
 import axios from "axios";
+import { IPagination } from "../../interfaces/IPagination";
 
 const ListRestaurant = () => {
   const [restaurants, setRestaurant] = useState<IRestaurant[]>([]);
+  const [nextPage, setNextPage] = useState("");
 
   useEffect(() => {
     axios
-      .get("http://localhost:8000/api/v1/restaurantes/")
+      .get<IPagination<IRestaurant>>(
+        "http://localhost:8000/api/v1/restaurantes/"
+      )
       .then((res) => {
-        setRestaurant(res.data.results);
+        setRestaurant([...restaurants, ...res.data.results]);
+        setNextPage(res.data.next);
       })
       .catch((err) => {
         console.error(err);
       });
   }, []);
+
+  const showMore = () => {
+    axios
+      .get<IPagination<IRestaurant>>(nextPage)
+      .then((res) => {
+        setRestaurant(res.data.results);
+        setNextPage(res.data.next);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   return (
     <section className={style.ListaRestaurantes}>
@@ -26,6 +43,7 @@ const ListRestaurant = () => {
       {restaurants?.map((item) => (
         <Restaurant restaurante={item} key={item.id} />
       ))}
+      {nextPage && <button onClick={showMore}> Ver mais </button>}
     </section>
   );
 };
